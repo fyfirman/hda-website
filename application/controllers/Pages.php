b5e8af1faf0d0fcf048ba3f3ff1b184a111501e9
Arif R Gilang <36944464+arifrgilang@users.noreply.github.com>
2018-07-18 20:51:16 +0700

    ganti landingpage

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -1,13 +1,14 @@
 <?php
     class Pages extends CI_Controller{
-        public function view($page = 'homepage'){
+        public function view($page = 'loginpage'){
             if(!file_exists(APPPATH.'views/pages/'.$page.'.php')){
                 show_404();
             }
 
             $data['title'] = ucfirst($page);
 
-            $this->load->view('templates/header');
+            // $this->load->view('templates/header');
+            $this->load->view('templates/header-loginpage');
             $this->load->view('pages/'.$page, $data);
             $this->load->view('templates/footer');
         }
---
+++ application/views/templates/header-loginpage.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Himatif Database</title>
+	
+	<link href="<?php echo base_url()?>/assets/css/bootstrap.min.css" rel="stylesheet">
+	<link href="<?php echo base_url()?>/assets/css/style.css" rel="stylesheet">
+
+	<script src="<?php echo base_url()?>/assets/js/jquery-3.3.1.min.js"></script>  
+    <script src="<?php echo base_url()?>/assets/js/bootstrap.min.js"></script> 
+</head>
+<body>
+<!-- Start of navbar -->
+	<nav class="navbar navbar-expand-md bg-dark navbar-dark fixed-top" >
+		<div class="container">
+			<a class="navbar-brand" href="#">Himatif Database</a>
+		</div>
+	</nav>
+	<!-- End of Navbar -->
