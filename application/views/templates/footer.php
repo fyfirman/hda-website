81d8cb991b051621dc4a57a8036cf040a670f505
atjhoendz <achunarmando@gmail.com>
2018-07-27 10:19:54 +0700

    change class navbar to footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,18 +1,14 @@
-
-	</div>
 	<!-- End of body content -->
 
 <!-- Footer -->
-	<footer class="navbar bg-dark text-white fixed-bottom">
+	<footer class="footer bg-dark text-white fixed-bottom">
 		<div class="container">
-			<!-- <div class="col-md-6"> -->
 				<span>
 					&copy; Tim HDA Delphi, 2018. Himatif FMIPA Unpad
 				</span>
 				<span class="float-right">
 					<a href="#">Credit</a>
-				</span> 
-			<!-- </div>			 -->
+				</span>
 		</div>
 	</footer>
 </body>
