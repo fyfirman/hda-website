698bfeb80b98a94d7677595c9c3f49406e5f0c6e
atjhoendz <achunarmando@gmail.com>
2018-07-27 10:17:57 +0700

    remove load database and adding autoload database library

--- application/models/Bk_model.php
+++ application/models/Bk_model.php
@@ -1,9 +1,5 @@
 <?php
     class Bk_model extends CI_Model{
-        public function __construct(){
-            $this->load->database();
-        }
-
         // fungsi ini mengembalikan data, be/dpa/mubes
         public function get_bk($what = NULL){
             return $this->db->select('*')
