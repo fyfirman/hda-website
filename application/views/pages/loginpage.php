3e6eaef465ee42d71ccec0ce0f9dc54cfe61f1c9
Firmansyah Yanuar <fyfirman@gmail.com>
2018-08-22 07:20:02 +0700

    Change naming convention

--- application/views/pages/loginpage.php
+++ application/views/pages/loginpage.php
@@ -1,18 +1,16 @@
-<!-- Jumbotron -->
+ <!-- Main Content Container -->
 <main role="main">
     <div class="jumbotron jumbotron-fluid mb-0">
         <div class="container d-flex align-items-center h-100">
-            <!-- Row -->
             <div class="row">
-                <!-- Column untuk teks awal-->
+                <!-- Title Text -->
                 <div class="col-md-8">
-                    <h1>Welcome to HDA</h1>
+                    <h1>Selamat datang di HDA</h1>
                     <p>Himatif Database Application adalah database online yang berisikan informasi mengenai Mahasiswa S-1 Teknik Informatika Unpad</p>
                 </div>
     
-                <!-- Column untuk form login-->
+                <!-- Login Form Container-->
                 <div class="col-md-4">
-                    <!-- Form -->
                     <form method="POST" action="<?php echo site_url('login/ceklogin') ?>">
                         <!-- NPM -->
                         <div class="form-group">
@@ -22,25 +20,21 @@
     
                         <!-- Password -->
                         <div class="form-group">
-                            <label for="pwd">Password</label>
-                            <input type="password" class="form-control" id="pwd" placeholder="Masukkan password" name="password">
+                            <label for="password">Password</label>
+                            <input type="password" class="form-control" id="password" placeholder="Masukkan password" name="password">
                         </div>
     
                         <!-- Checkbox 'Remember me'-->
                         <div class="form-group form-check">
                             <label class="form-check-label">
-                                <input class="form-check-input" type="checkbox" name="remember"> Remember me
+                                <input class="form-check-input" type="checkbox" name="remember-me">Ingat Saya
                             </label>
                         </div>
     
-                        <!-- Submit buton -->
-                        <button type="submit" class="btn btn-primary">login</button>
-    
-                        <!-- button sementara -->
-                        <!-- <a class="btn btn-primary" href="<?php echo site_url('/bk'); ?>">Login</a> -->
+                        <!-- Submit Button -->
+                        <button type="submit" class="btn btn-primary">Masuk</button>
                     </form>
                 </div>
-    
             </div>
         </div>
     </div>
