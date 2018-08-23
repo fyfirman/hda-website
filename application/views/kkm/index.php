57df248aeaf084627fd09b9ff4cb4d03aba73a3a
Firmansyah Yanuar <fyfirman@gmail.com>
2018-08-23 10:05:13 +0700

    Add KKM layout

---
+++ application/views/kkm/index.php
@@ -0,0 +1,107 @@
+<main role="main">
+	<div class="card-post my-5">
+		<div class="container p-5 bg-light border">
+			<div class="row">
+				<div class="col-12">
+					<h1>Teknologi</h1>
+					<div class="row">
+						<div class="col-lg-4">
+							<div class="card card-shadow d-flex">
+								<div class="card-header text-center">
+									<img src="<?php echo site_url('images/status_done.jpg'); ?>" alt="" class="kkm-logo">
+									Nama KKM
+								</div>
+								<div class="card-body">Content</div>
+								<div class="card-footer text-right">
+									<img src="<?php echo site_url('images/status_done.jpg'); ?>" alt="" class="rounded-circle kkm-chief">
+									Ketua
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-4">
+							<div class="card card-shadow d-flex">
+								<div class="card-header text-center">
+									<img src="<?php echo site_url('images/status_done.jpg'); ?>" alt="" class="kkm-logo">
+									Nama KKM
+								</div>
+								<div class="card-body">Content</div>
+								<div class="card-footer text-right">
+									<img src="<?php echo site_url('images/status_done.jpg'); ?>" alt="" class="rounded-circle kkm-chief">
+									Ketua
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-4">
+							<div class="card card-shadow d-flex">
+								<div class="card-header text-center">
+									<img src="<?php echo site_url('images/status_done.jpg'); ?>" alt="" class="kkm-logo">
+									Nama KKM
+								</div>
+								<div class="card-body">Content</div>
+								<div class="card-footer text-right">
+									<img src="<?php echo site_url('images/status_done.jpg'); ?>" alt="" class="rounded-circle kkm-chief">
+									Ketua
+								</div>
+							</div>
+						</div>
+					</div>
+					<h1 class="mt-5">Olahraga</h1>
+					<div class="row">
+						<div class="col-lg-6 mb-3">
+							<div class="card card-shadow d-flex">
+								<div class="card-header text-center">
+									<img src="<?php echo site_url('images/status_done.jpg'); ?>" alt="" class="kkm-logo">
+									Nama KKM
+								</div>
+								<div class="card-body">Content</div>
+								<div class="card-footer text-right">
+									<img src="<?php echo site_url('images/status_done.jpg'); ?>" alt="" class="rounded-circle kkm-chief">
+									Ketua
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-6">
+							<div class="card card-shadow d-flex">
+								<div class="card-header text-center">
+									<img src="<?php echo site_url('images/status_done.jpg'); ?>" alt="" class="kkm-logo">
+									Nama KKM
+								</div>
+								<div class="card-body">Content</div>
+								<div class="card-footer text-right">
+									<img src="<?php echo site_url('images/status_done.jpg'); ?>" alt="" class="rounded-circle kkm-chief">
+									Ketua
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-6">
+							<div class="card card-shadow d-flex">
+								<div class="card-header text-center">
+									<img src="<?php echo site_url('images/status_done.jpg'); ?>" alt="" class="kkm-logo">
+									Nama KKM
+								</div>
+								<div class="card-body">Content</div>
+								<div class="card-footer text-right">
+									<img src="<?php echo site_url('images/status_done.jpg'); ?>" alt="" class="rounded-circle kkm-chief">
+									Ketua
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-6">
+							<div class="card card-shadow d-flex">
+								<div class="card-header text-center">
+									<img src="<?php echo site_url('images/status_done.jpg'); ?>" alt="" class="kkm-logo">
+									Nama KKM
+								</div>
+								<div class="card-body">Content</div>
+								<div class="card-footer text-right">
+									<img src="<?php echo site_url('images/status_done.jpg'); ?>" alt="" class="rounded-circle kkm-chief">
+									Ketua
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>		
+		</div>
+	</div>
+</main>
