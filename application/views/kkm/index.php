8dab0aa6d285488cf1523816cff8ae3138372aa8
dimassatriap <prakosodimad@gmail.com>
2018-08-24 16:30:16 +0700

    Add Content to KKM Olahraga

--- application/views/kkm/index.php
+++ application/views/kkm/index.php
@@ -9,7 +9,7 @@
 							<div class="card card-shadow d-flex">
 								<div class="card-header text-center">
 									<img src="<?php echo site_url('images/Logo_Assets/devcom.png'); ?>" alt="" class="kkm-logo">
-									DEVCOM
+									&nbsp; DEVCOM
 								</div>
 								<div class="card-body kkm-content">
                                     <div class="kkm-content-inner">
@@ -27,7 +27,7 @@
 							<div class="card card-shadow d-flex">
 								<div class="card-header text-center">
 									<img src="<?php echo site_url('images/Logo_Assets/idea.png'); ?>" alt="" class="kkm-logo">
-								    IDEA
+								    &nbsp; IDEA
 								</div>
 								<div class="card-body kkm-content">
                                     <div class="kkm-content-inner">
@@ -49,7 +49,7 @@
 								</div>
 								<div class="card-body kkm-content">
                                    <div class="kkm-content-inner">
-                                       Informatics Robotics Club (IRC) adalah Kegiatan Kelompok Mahasiswa dalam bidang robotika. 
+                                       Informatics Robotic Club (IRC) adalah Kegiatan Kelompok Mahasiswa dalam bidang robotika. 
                                     </div>
                                 </div>
 								<div class="card-footer text-right">
@@ -62,58 +62,61 @@
 					</div>
 					<h1 class="mt-5">Olahraga</h1>
 					<div class="row">
-						<div class="col-lg-6 mb-3">
+							<div class="col-lg-4">
 							<div class="card card-shadow d-flex">
 								<div class="card-header text-center">
-									<img src="<?php echo site_url('images/status_done.jpg'); ?>" alt="" class="kkm-logo">
-									Nama KKM
+									<img src="<?php echo site_url('images/Logo_Assets/ibc.png'); ?>" alt="" class="kkm-logo">
+									&nbsp; IBC
 								</div>
-								<div class="card-body">Content</div>
+								<div class="card-body kkm-content">
+                                    <div class="kkm-content-inner">
+                                       Informatics Basketball Club (IBC) adalah Kegiatan Kelompok Mahasiswa dalam bidang olahraga basket.
+                                    </div>
+								</div>
 								<div class="card-footer text-right">
-									<img src="<?php echo site_url('images/status_done.jpg'); ?>" alt="" class="rounded-circle kkm-chief">
-									Ketua
+								    Ketua : 
+									<!--<img src="http://hda.himatif.org/assets/foto/2016/18.jpg" alt="" class="rounded-circle kkm-chief">-->
+									Erlangga Riyyan Nugraha
+								</div>
+							</div>    
+						</div>
+						<div class="col-lg-4">
+							<div class="card card-shadow d-flex">
+								<div class="card-header text-center">
+									<img src="<?php echo site_url('images/Logo_Assets/ibt.png'); ?>" alt="" class="kkm-logo">
+									&nbsp; IBT
+								</div>
+								<div class="card-body kkm-content">
+                                   <div class="kkm-content-inner">
+                                       Informatics Badminton Club (IBT) adalah Kegiatan Kelompok Mahasiswa dalam bidang olahraga bulu tangkis. 
+                                    </div>
+                                </div>
+								<div class="card-footer text-right">
+								    Ketua :
+									<!--<img src="http://hda.himatif.org/assets/foto/2016/11.jpg" alt="" class="rounded-circle kkm-chief">-->
+									Muhammad Fariz
 								</div>
 							</div>
 						</div>
-						<div class="col-lg-6">
+						<div class="col-lg-4">
 							<div class="card card-shadow d-flex">
 								<div class="card-header text-center">
-									<img src="<?php echo site_url('images/status_done.jpg'); ?>" alt="" class="kkm-logo">
-									Nama KKM
+									<img src="<?php echo site_url('images/Logo_Assets/ifc.png'); ?>" alt="" class="kkm-logo">
+								    &nbsp; IFC
 								</div>
-								<div class="card-body">Content</div>
+								<div class="card-body kkm-content">
+                                    <div class="kkm-content-inner">
+                                       Informatics Futsal Club (IFC) adalah Kegiatan Kelompok Mahasiswa dalam bidang olahraga futsal. 
+                                    </div>
+                                </div>
 								<div class="card-footer text-right">
-									<img src="<?php echo site_url('images/status_done.jpg'); ?>" alt="" class="rounded-circle kkm-chief">
-									Ketua
+								    Ketua : 
+									<!--<img src="http://hda.himatif.org/assets/foto/2016/23.jpg" alt="" class="rounded-circle kkm-chief">-->
+									Shaleh Husen Attamimi
 								</div>
 							</div>
 						</div>
-						<div class="col-lg-6">
-							<div class="card card-shadow d-flex">
-								<div class="card-header text-center">
-									<img src="<?php echo site_url('images/status_done.jpg'); ?>" alt="" class="kkm-logo">
-									Nama KKM
-								</div>
-								<div class="card-body">Content</div>
-								<div class="card-footer text-right">
-									<img src="<?php echo site_url('images/status_done.jpg'); ?>" alt="" class="rounded-circle kkm-chief">
-									Ketua
-								</div>
-							</div>
-						</div>
-						<div class="col-lg-6">
-							<div class="card card-shadow d-flex">
-								<div class="card-header text-center">
-									<img src="<?php echo site_url('images/status_done.jpg'); ?>" alt="" class="kkm-logo">
-									Nama KKM
-								</div>
-								<div class="card-body">Content</div>
-								<div class="card-footer text-right">
-									<img src="<?php echo site_url('images/status_done.jpg'); ?>" alt="" class="rounded-circle kkm-chief">
-									Ketua
-								</div>
-							</div>
-						</div>
+					</div>
 					</div>
 				</div>
 			</div>		
