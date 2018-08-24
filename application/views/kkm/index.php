c23e38743bcad789217cbabb5ab14d87617f2288
dimassatriap <prakosodimad@gmail.com>
2018-08-24 16:14:20 +0700

    Add Content to KKM Teknologi

--- application/views/kkm/index.php
+++ application/views/kkm/index.php
@@ -8,39 +8,54 @@
 						<div class="col-lg-4">
 							<div class="card card-shadow d-flex">
 								<div class="card-header text-center">
-									<img src="<?php echo site_url('images/status_done.jpg'); ?>" alt="" class="kkm-logo">
-									Nama KKM
+									<img src="<?php echo site_url('images/Logo_Assets/devcom.png'); ?>" alt="" class="kkm-logo">
+									DEVCOM
 								</div>
-								<div class="card-body">Content</div>
+								<div class="card-body kkm-content">
+                                    <div class="kkm-content-inner">
+                                       	Developer Community (DevCom) adalah Kegiatan Kelompok Mahasiswa dalam bidang pengembangan aplikasi. 
+                                    </div>
+								</div>
 								<div class="card-footer text-right">
-									<img src="<?php echo site_url('images/status_done.jpg'); ?>" alt="" class="rounded-circle kkm-chief">
-									Ketua
+								    Ketua : 
+									<!--<img src="http://hda.himatif.org/assets/foto/2016/54.jpg" alt="" class="rounded-circle kkm-chief">-->
+									Ibnu Ahsani
+								</div>
+							</div>    
+						</div>
+						<div class="col-lg-4">
+							<div class="card card-shadow d-flex">
+								<div class="card-header text-center">
+									<img src="<?php echo site_url('images/Logo_Assets/idea.png'); ?>" alt="" class="kkm-logo">
+								    IDEA
+								</div>
+								<div class="card-body kkm-content">
+                                    <div class="kkm-content-inner">
+                                       Informatics Design Association (Idea) adalah Kegiatan Kelompok Mahasiswa dalam bidang Desain. 
+                                    </div>
+                                </div>
+								<div class="card-footer text-right">
+								    Ketua : 
+									<!--<img src="http://hda.himatif.org/assets/foto/2016/37.jpg" alt="" class="rounded-circle kkm-chief">-->
+									Rafidghadah Damarta
 								</div>
 							</div>
 						</div>
 						<div class="col-lg-4">
 							<div class="card card-shadow d-flex">
 								<div class="card-header text-center">
-									<img src="<?php echo site_url('images/status_done.jpg'); ?>" alt="" class="kkm-logo">
-									Nama KKM
+									<img src="<?php echo site_url('images/Logo_Assets/irc-dummy.png'); ?>" alt="" class="kkm-logo">
+									<!-- IRC -->
 								</div>
-								<div class="card-body">Content</div>
+								<div class="card-body kkm-content">
+                                   <div class="kkm-content-inner">
+                                       Informatics Robotics Club (IRC) adalah Kegiatan Kelompok Mahasiswa dalam bidang robotika. 
+                                    </div>
+                                </div>
 								<div class="card-footer text-right">
-									<img src="<?php echo site_url('images/status_done.jpg'); ?>" alt="" class="rounded-circle kkm-chief">
-									Ketua
-								</div>
-							</div>
-						</div>
-						<div class="col-lg-4">
-							<div class="card card-shadow d-flex">
-								<div class="card-header text-center">
-									<img src="<?php echo site_url('images/status_done.jpg'); ?>" alt="" class="kkm-logo">
-									Nama KKM
-								</div>
-								<div class="card-body">Content</div>
-								<div class="card-footer text-right">
-									<img src="<?php echo site_url('images/status_done.jpg'); ?>" alt="" class="rounded-circle kkm-chief">
-									Ketua
+								    Ketua :
+									<!--<img src="http://hda.himatif.org/assets/foto/2017/65.jpg" alt="" class="rounded-circle kkm-chief">-->
+									Yoga Prambudi S.P.
 								</div>
 							</div>
 						</div>
