72a991cb45cf69807f50d4c3884878152d2d71ab
atjhoendz <achunarmando@gmail.com>
2018-08-27 01:36:19 +0700

    move ke controller Page supaya efisien

--- application/controllers/Kkm.php
+++
@@ -1,9 +0,0 @@
-<?php
-	class Kkm extends CI_Controller{
-		public function index(){
-			$this->load->view('templates/header');
-			$this->load->view('kkm/index');
-			$this->load->view('templates/footer');
-		}
-	}
-?>
