785bc4eaacf8cb695471d850f7c2b10df41e8e63
Firmansyah Yanuar <fyfirman@gmail.com>
2018-09-02 13:18:38 +0700

    Add anggota selection

--- application/controllers/Anggota.php
+++ application/controllers/Anggota.php
@@ -4,10 +4,25 @@
             parent::__construct();
             // $this->load->model('m_user');
         }
-        public function view($angkatan = NULL){
+
+        public function view($tahun_angkatan = NULL){
 			$this->load->view('templates/header');
-            $this->load->view('anggota/index');
+            if($tahun_angkatan == NULL){
+            	$this->view_selection();
+            }
+            else{
+            	$this->view_angkatan($tahun_angkatan);
+            }
             $this->load->view('templates/footer');
         }
+
+        private function view_selection(){
+        	$this->load->view('anggota/selection');
+        }
+
+        private function view_angkatan($tahun_angkatan){
+        	#Under Contruction
+        	$this->load->view("anggota/angkatan");
+        } 
 	}
 ?>
---
+++ application/views/anggota/selection.php
@@ -0,0 +1,27 @@
+<main role="main" class="nav-margin">
+	<div class="card-post my-5">
+		<div class="container p-5 bg-light border">
+			<div class="row">
+				<div class="col-12">
+					<h1> Delphi </h1>
+					<div class="row">
+						<?php for ($i = 0; $i < 65; $i++) : ?>
+						<div class="col-lg-3 mb-2">
+							<div class="card card-shadow d-flex">
+								<div class="card-header">
+									<img src="http://hda.himatif.org/assets/foto/2017/51.jpg" alt="" class="mw-100 mh-100">
+								</div>
+								<div class="card-body text-center">
+									140810170051
+									<br>
+									Firmansyah Yanuar
+								</div>
+							</div>
+						</div>
+						<?php endfor ?>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</main>
