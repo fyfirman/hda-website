06eb3641d64c445fa26597cdd41f977f9a4bfa51
Firmansyah Yanuar <fyfirman@gmail.com>
2018-09-05 20:28:33 +0700

    Make selection angkatan page static

--- application/views/anggota/selection.php
+++ application/views/anggota/selection.php
@@ -4,9 +4,62 @@
 			<div class="row">
 				<div class="col-12">
 					<div class="row">
-						<?php for ($i = 0; $i < 8; $i++) : ?>
 						<div class="col-lg-6 mb-5">
 							<a href="<?php echo site_url('anggota/2012'); ?>">
+							<div class="thumbnail text-center">
+								<img class="mh-100 mw-100" src="<?php echo site_url('images/fotang.jpg'); ?>" alt="" >
+								<div class="text-overlay selection-angkatan px-1 ">
+									<p>CIRCLE</p>
+								</div>
+							</div>
+							</a>
+						</div>
+						<div class="col-lg-6 mb-5">
+							<a href="<?php echo site_url('anggota/2013'); ?>">
+							<div class="thumbnail text-center">
+								<img class="mh-100 mw-100" src="<?php echo site_url('images/fotang.jpg'); ?>" alt="" >
+								<div class="text-overlay selection-angkatan px-1 ">
+									<p>PASCAL</p>
+								</div>
+							</div>
+							</a>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-lg-6 mb-5">
+							<a href="<?php echo site_url('anggota/2014'); ?>">
+							<div class="thumbnail text-center">
+								<img class="mh-100 mw-100" src="<?php echo site_url('images/fotang.jpg'); ?>" alt="" >
+								<div class="text-overlay selection-angkatan px-1 ">
+									<p>ASSEMBLY</p>
+								</div>
+							</div>
+							</a>
+						</div>
+						<div class="col-lg-6 mb-5">
+							<a href="<?php echo site_url('anggota/2015'); ?>">
+							<div class="thumbnail text-center">
+								<img class="mh-100 mw-100" src="<?php echo site_url('images/fotang.jpg'); ?>" alt="" >
+								<div class="text-overlay selection-angkatan px-1 ">
+									<p>BINARY</p>
+								</div>
+							</div>
+							</a>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-lg-6 mb-5">
+							<a href="<?php echo site_url('anggota/2016'); ?>">
+							<div class="thumbnail text-center">
+								<img class="mh-100 mw-100" src="<?php echo site_url('images/fotang.jpg'); ?>" alt="" >
+								<div class="text-overlay selection-angkatan px-1 ">
+									<p>CYBER</p>
+								</div>
+							</div>
+							</a>
+						</div>
+						<div class="col-lg-6 mb-5">
+							<a href="<?php echo site_url('anggota/2017'); ?>">
 							<div class="thumbnail text-center">
 								<img class="mh-100 mw-100" src="<?php echo site_url('images/fotang.jpg'); ?>" alt="" >
 								<div class="text-overlay selection-angkatan px-1 ">
@@ -15,7 +68,18 @@
 							</div>
 							</a>
 						</div>
-						<?php endfor ?>
+					</div>
+					<div class="row">
+						<div class="col-lg-6 mb-5">
+							<a href="<?php echo site_url('anggota/2018'); ?>">
+							<div class="thumbnail text-center">
+								<img class="mh-100 mw-100" src="<?php echo site_url('images/fotang.jpg'); ?>" alt="" >
+								<div class="text-overlay selection-angkatan px-1 ">
+									<p>ECLIPSE</p>
+								</div>
+							</div>
+							</a>
+						</div>
 					</div>
 				</div>
 			</div>
