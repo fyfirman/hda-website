4b50c30909ce004539e395119b38cf015ecf30f7
Firmansyah Yanuar <fyfirman@gmail.com>
2018-09-05 20:20:17 +0700

    Add link anggota angkatan

--- application/views/anggota/selection.php
+++ application/views/anggota/selection.php
@@ -5,13 +5,15 @@
 				<div class="col-12">
 					<div class="row">
 						<?php for ($i = 0; $i < 8; $i++) : ?>
-						<div class="col-lg-6 mb-5">	
+						<div class="col-lg-6 mb-5">
+							<a href="<?php echo site_url('anggota/2012'); ?>">
 							<div class="thumbnail text-center">
 								<img class="mh-100 mw-100" src="<?php echo site_url('images/fotang.jpg'); ?>" alt="" >
-								<div class="tulis px-1 ">
+								<div class="text-overlay selection-angkatan px-1 ">
 									<p>DELPHI</p>
 								</div>
 							</div>
+							</a>
 						</div>
 						<?php endfor ?>
 					</div>
