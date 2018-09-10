b4c2caf64ac309998df9476af852371e1e84bf06
Dimas Satria <prakosodimad@gmail.com>
2018-09-10 16:28:45 +0700

    Create View detail-anggota pt.1

--- application/views/anggota/detail-anggota.php
+++ application/views/anggota/detail-anggota.php
@@ -1,7 +1,67 @@
 <main role="main" class="nav-margin">
 	<div class="card-post my-5">
 		<div class="container p-5 bg-light border">
-				
+            <div class="row">
+                <div class="col-lg-3">
+                    <div class="img-responsive mw-100 ">
+                        <img src="http://hda.himatif.org/assets/foto/2017/07.jpg" alt="" class="mw-100 mh-100" />   
+                    </div>
+                </div>
+                <div class="col-lg-9">
+                   
+                    <form>
+                      <div class="form-group row">
+                        <label for="inputNPM" class="col-sm-1 col-form-label">NPM</label>
+                        <div class="col-sm-3">
+                          <input type="text" class="form-control" id="NPMAnggota" placeholder="NPM">
+                        </div>
+                        
+                        <label for="inputNama" class="col-sm-1 col-form-label">Nama</label>
+                        <div class="col-sm-5">
+                          <input type="text" class="form-control" id="namaAnggota" placeholder="Nama">
+                        </div>
+                      </div>   
+                       
+                      <div class="form-group row">
+                       
+                        <label for="inputJK" class="col-sm-1 col-form-label">JK</label>
+                        <div class="form-check form-check-inline">
+                          <input class="form-check-input" type="radio" name="pilihanJK" id="JK-Laki" value="JK-L">
+                          <label class="form-check-label" for="JK-Laki">L</label>
+                        </div>
+                        
+                        <div class="form-check form-check-inline">
+                          <input class="form-check-input" type="radio" name="pilihanJK" id="JK-Perempuan" value="JK-P">
+                          <label class="form-check-label" for="JK-Laki">P</label>
+                        </div>
+                         
+                        <label for="inputGolDar" class="col-sm-1 col-form-label">GolDar</label>
+                        <div class="col-sm-2">
+                            <select class="form-control" id="GolDarAnggota">
+                              <option>A+</option>
+                              <option>A-</option>
+                              <option>B+</option>
+                              <option>B-</option>
+                              <option>AB+</option>
+                              <option>AB-</option>
+                              <option>O+</option>
+                              <option>O-</option>
+                            </select>
+                        </div>
+                          
+                      </div>
+                      
+                      <div class="form-group row">
+                        <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
+                        <div class="col-sm-10">
+                          <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+                        </div>
+                      </div>
+                    </form>                    
+                    
+                    
+                </div>
+            </div>
 		</div>
-    </div>
+    </div>  
 </main>
